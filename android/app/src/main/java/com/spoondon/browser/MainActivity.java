60f8636d1f8609f9333b6a1b9c5c5b8b6509ffcd
Spoon <spoondon@users.noreply.github.com>
2026-06-07 04:06:46 +0530

    Upgrade to native WebView browser engine

--- android/app/src/main/java/com/spoondon/browser/MainActivity.java
+++ android/app/src/main/java/com/spoondon/browser/MainActivity.java
@@ -1,5 +1,41 @@
 package com.spoondon.browser;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // Start page (you can change later)
+        webView.loadUrl("https://www.google.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
