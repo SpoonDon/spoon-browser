aadc613f64254af1a67cad5bb7350a6b1c67b905
Spoon <spoondon@users.noreply.github.com>
2026-06-11 04:02:37 +0530

    Make browser item adapter extend ArrayAdapter

--- android/app/src/main/java/com/spoondon/browser/BrowserItemAdapter.java
+++ android/app/src/main/java/com/spoondon/browser/BrowserItemAdapter.java
@@ -1,5 +1,22 @@
 package com.spoondon.browser;
 
-public class BrowserItemAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
+public class BrowserItemAdapter
+        extends ArrayAdapter<
+                MainActivity.BrowserItem> {
+
+    public BrowserItemAdapter(
+            Context context,
+            ArrayList<MainActivity.BrowserItem> items) {
+
+        super(
+                context,
+                0,
+                items
+        );
+    }
 }
