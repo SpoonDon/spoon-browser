d8536c7c95f0d3d33cd8d46182516fdb120753e7
SpoonDon <SpoonDon@users.noreply.github.com>
2026-06-24 04:33:12 +0530

    Fix autofill startup crash loop

--- android/app/src/main/java/com/spoondon/browser/MainActivity.java
+++ android/app/src/main/java/com/spoondon/browser/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(androidx.appcompat.R.style.Theme_AppCompat_NoActionBar);
         super.onCreate(null);
-,
+
         filePickerLauncher = registerForActivityResult(
            new ActivityResultContracts.GetContent(),
                 uri -> {
