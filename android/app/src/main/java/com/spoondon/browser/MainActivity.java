149cde837cca0673a433ca150b0a326446f9ae90
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-07-03 00:12:58 +0530

    Fix: Prune duplicate widget engine import declarations.

--- android/app/src/main/java/com/spoondon/browser/MainActivity.java
+++ android/app/src/main/java/com/spoondon/browser/MainActivity.java
@@ -37,9 +37,6 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.PopupMenu;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.text.TextUtils;
 import android.app.AlertDialog;
 
