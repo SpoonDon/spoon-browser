7f71e028d61b7a190ffbe09955d616f2780c8cc7
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-07-08 23:29:52 +0530

    Fix Tabs DB update on page navigation
    Ensure Tabs DB is updated when navigating to a new page.

--- android/app/src/main/java/com/spoondon/browser/SpoonWebViewClient.java
+++ android/app/src/main/java/com/spoondon/browser/SpoonWebViewClient.java
@@ -176,6 +176,11 @@ public class SpoonWebViewClient extends WebViewClient {
                     lastRecordedHistoryTime = currentTime;
                 }
             }
+            
+            // BUG FIX: Update the Tabs DB whenever the user actually navigates to a new page!
+            if (activity != null) {
+                activity.saveTabsState();
+            }
         }
     }
 
