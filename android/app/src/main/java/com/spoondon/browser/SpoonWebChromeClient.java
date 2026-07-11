fe011f4f9ec6dc03b745b61a3b2eb82e6466083b
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-07-11 14:53:07 +0530

    Update SpoonWebChromeClient.java

--- android/app/src/main/java/com/spoondon/browser/SpoonWebChromeClient.java
+++ android/app/src/main/java/com/spoondon/browser/SpoonWebChromeClient.java
@@ -1,10 +1,12 @@
 package com.spoondon.browser;
 
 import android.os.Build;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -15,6 +17,32 @@ public class SpoonWebChromeClient extends WebChromeClient {
 
     public SpoonWebChromeClient(MainActivity activity) {
         this.activity = activity;
+    }
+
+    @Override
+    public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+        WebView dummyWebView = new WebView(view.getContext());
+        dummyWebView.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView tempView, android.webkit.WebResourceRequest request) {
+                String rawUrl = request.getUrl().toString();
+                
+                if (rawUrl.contains(" ") && (rawUrl.contains("http://") || rawUrl.contains("https://"))) {
+                    int httpIndex = rawUrl.indexOf("http");
+                    if (httpIndex != -1) {
+                        rawUrl = rawUrl.substring(httpIndex).trim();
+                    }
+                }
+                
+                view.loadUrl(rawUrl);
+                return true; 
+            }
+        });
+        
+        WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+        transport.setWebView(dummyWebView);
+        resultMsg.sendToTarget();
+        return true;
     }
 
     @Override
@@ -58,7 +86,6 @@ public class SpoonWebChromeClient extends WebChromeClient {
         List<String> allowedResources = new ArrayList<>();
 
         for (String resource : request.getResources()) {
-            // Strict Firewall Rules: YouTube DRM allowed, absolute privacy for camera/mic hooks
             if (resource.equals(PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID)) {
                 if (host.endsWith("youtube.com") || 
                     host.endsWith("googlevideo.com") || 
